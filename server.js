02f06bfaad939a0990d029ab989e0d4f7d281c8c
virtuxa <dmitry-vidineeff@ya.ru>
2024-10-13 15:31:22 +0300

    Связали формы с БД

--- server.js
+++ server.js
@@ -33,6 +33,45 @@ pool.connect((err, client, release) => {
 // -------------------------------------------------------
 
 // Обработка форм
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Регистрация пользователя
+app.post('/register', async (req, res) => {
+    const { email, username, displayName, password, day, month, year } = req.body;
+    const dateOfBirth = `${year}-${month}-${day}`; // Форматируем дату рождения
+  
+    try {
+      const result = await pool.query(
+        'INSERT INTO users (email, username, display_name, password, date_of_birth) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+        [email, username, displayName, password, dateOfBirth]
+      );
+      res.redirect('/login'); // После успешной регистрации перенаправляем на страницу входа
+    } catch (err) {
+      console.error(err);
+      res.send('Ошибка регистрации');
+    }
+  });
+  
+  // Авторизация пользователя
+  app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const result = await pool.query(
+        'SELECT * FROM users WHERE email = $1 AND password = $2',
+        [email, password]
+      );
+  
+      if (result.rows.length > 0) {
+        res.redirect('/home');
+      } else {
+        res.send('Неправильный e-mail или пароль');
+      }
+    } catch (err) {
+      console.error(err);
+      res.send('Ошибка входа');
+    }
+  });
 
 // -------------------------------------------------------
 
--- static/register/register.js
+++ static/register/register.js
@@ -5,3 +5,18 @@ function showDescription() {
 function hideDescription() {
     document.getElementById('description-text').style.display = 'none';
 }
+
+function checkPasswords() {
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+    const errorMessage = document.getElementById('error-message');
+  
+    if (password !== confirmPassword) {
+      errorMessage.style.display = 'block';
+      return false; // Предотвращает отправку формы
+    } else {
+      errorMessage.style.display = 'none';
+      return true; // Разрешает отправку формы
+    }
+  }
+  
