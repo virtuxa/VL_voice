fec3a137011ff1d88826558707f6185c1a46bffc
virtuxa <dmitry-vidineeff@ya.ru>
2024-10-13 12:36:43 +0300

    Начинаем тестить

--- server.js
+++ server.js
@@ -3,17 +3,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Маршрут для файла server.css
-app.get('/server.css', (req, res) => {
-    res.sendFile(path.join(__dirname, 'server.css'));
-});
-
 // Обслуживание статических файлов из папки static
 app.use(express.static(path.join(__dirname, 'static')));
 
-// Отправка server.html при запросе к корню
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'server.html'));
+// Для всех маршрутов загружаем server.html
+app.get(['/home', '/login', '/register'], (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'server.html'));
 });
 
 // Запуск сервера
