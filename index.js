c965a61277bef2b88044c8f214af5218707d6248
virtuxa <dmitry-vidineeff@ya.ru>
2024-10-13 17:04:46 +0300

    Рабочая версия авторизации и регистрации

--- index.js
+++ index.js
@@ -92,17 +92,6 @@ app.get(['/register'], (req, res) => {
 
 // -------------------------------------------------------
 
-function loadContent(page) {
-    fetch(page)
-        .then(response => response.text())
-        .then(html => {
-            document.querySelector('.thirdbar').innerHTML = html;
-        })
-        .catch(error => console.log('Ошибка загрузки контента:', error));
-}
-
-// -------------------------------------------------------
-
 // Запуск сервера
 app.listen(port, () => {
     console.log(`Сервер запущен на http://localhost:${port}`);
