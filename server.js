c2833dc093c98ce27c801dc841feed73fb1094f9
virtuxa <dmitry-vidineeff@ya.ru>
2024-10-13 14:43:07 +0300

    Корректно настроен и связан login и register

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const port = 3000;
 // Обслуживание статических файлов из папки static
 app.use(express.static(path.join(__dirname, 'static')));
 
-// Для всех маршрутов загружаем server.html
-app.get(['/home', '/login', '/register'], (req, res) => {
+// 
+app.get(['/home'], (req, res) => {
     res.sendFile(path.join(__dirname, 'static', 'server.html'));
+});
+
+// 
+app.get(['/login'], (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'login.html'));
+});
+app.get(['/register'], (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'register.html'));
 });
 
 // Запуск сервера
---
+++ static/register/register.js
@@ -0,0 +1,7 @@
+function showDescription() {
+    document.getElementById('description-text').style.display = 'block';
+}
+
+function hideDescription() {
+    document.getElementById('description-text').style.display = 'none';
+}
